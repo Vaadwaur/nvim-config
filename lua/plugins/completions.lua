93a7095f82391b4f0bb83df1cc83c8ffd51427d8
Jonas Hansson <jonas.ng.hansson@gmail.com>
2024-04-09 23:42:59 +0200

    remove snippets from completions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -2,26 +2,28 @@ return {
 	{
 		"hrsh7th/cmp-nvim-lsp",
 	},
-	{ "L3MON4D3/LuaSnip", dependencies = {
-		"saadparwaiz1/cmp_luasnip",
-		"rafamadriz/friendly-snippets",
-	} },
+	{ "onsails/lspkind.nvim" },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		}
+	},
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
-			--local lspkind = require("lspkind")
+			local lspkind = require("lspkind")
 
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			--vim.opt.completeopt = "menu,menuone,noselect"
+			--require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
-				snippet = {
+				--[[snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
 					end,
-				},
+				},]]
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
@@ -41,28 +43,18 @@ return {
 				-- sources for autocompletion
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" }, -- lsp
-					{ name = "luasnip" }, -- snippets
+					-- { name = "luasnip" }, -- snippets
 					{ name = "buffer" }, -- text within current buffer
 					{ name = "path" }, -- file system paths
 				}),
 				-- configure lspkind for vscode-like icons
-				--[[formatting = {
+				formatting = {
 					format = lspkind.cmp_format({
 						maxwidth = 50,
 						ellipsis_char = "...",
 					}),
-				},]]
+				},
 			})
 		end,
-		--[[dependencies = {
-			"onsails/lspkind.nvim",
-			{
-				"L3MON4D3/LuaSnip",
-				-- follow latest release
-				version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-				-- install jsregexp (optional!)
-				build = "make install_jsregexp",
-			},
-		},]]
 	},
 }
